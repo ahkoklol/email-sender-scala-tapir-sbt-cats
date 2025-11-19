4cd3575815ed68d1416d8a86bb314838befde223
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-11-19 17:49:50 +0400

    Initial commit

---
+++ mdistributions-back-cats/build.sbt
@@ -0,0 +1,18 @@
+val tapirVersion = "1.11.50"
+
+lazy val rootProject = (project in file(".")).settings(
+  Seq(
+    name := "mdistributions-back-cats",
+    version := "0.1.0-SNAPSHOT",
+    organization := "com.ahkoklol",
+    scalaVersion := "3.7.3",
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.tapir" %% "tapir-cats-effect" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-netty-server-cats" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion,
+      "ch.qos.logback" % "logback-classic" % "1.5.18",
+      "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % tapirVersion % Test,
+      "org.scalatest" %% "scalatest" % "3.2.19" % Test
+    )
+  )
+)
---
+++ mdistributions-back-cats/src/test/scala/com/ahkoklol/EndpointsSpec.scala
@@ -0,0 +1,32 @@
+package com.ahkoklol
+
+import com.ahkoklol.Endpoints.{*, given}
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.client3.testing.SttpBackendStub
+import sttp.client3.{UriContext, basicRequest}
+import sttp.tapir.server.stub.TapirStubInterpreter
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import sttp.tapir.integ.cats.effect.CatsMonadError
+
+class EndpointsSpec extends AnyFlatSpec with Matchers with EitherValues:
+
+  it should "return hello message" in {
+    // given
+    val backendStub = TapirStubInterpreter(SttpBackendStub(new CatsMonadError[IO]()))
+      .whenServerEndpointRunLogic(helloServerEndpoint)
+      .backend()
+
+    // when
+    val response = basicRequest
+      .get(uri"http://test.com/hello?name=adam")
+      .send(backendStub)
+
+    // then
+    response.map(_.body.value shouldBe "Hello adam").unwrap
+  }
+
+  extension [T](t: IO[T]) def unwrap: T = t.unsafeRunSync()
